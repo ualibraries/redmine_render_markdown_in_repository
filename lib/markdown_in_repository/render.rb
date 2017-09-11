838c020cdc1a1aa7f043e20ef513a354d45c3837
Nguyen Van Bach <vanbach1213@gmail.com>
2017-09-11 21:24:10 +0900

    Edit block_code to use Redmine buildin method

--- lib/markdown_in_repository/render.rb
+++ lib/markdown_in_repository/render.rb
@@ -8,13 +8,5 @@ module MarkdownInRepository
     Redmine::MimeType::EXTENSIONS[ext] = 'text/x-markdown'
   end
 
-  class MarkdownRender < Redcarpet::Render::HTML
-    def block_code(code, language)
-      # convert utf8 for CodeRay
-      code = Redmine::CodesetUtil.to_utf8_by_setting(code)
-      language = CodeRay::FileType[language] if language
-      language ||= :auto
-      CodeRay.scan(code, language).div
-    end
-  end
+  class MarkdownRender < Redmine::WikiFormatting::Markdown::HTML; end
 end
